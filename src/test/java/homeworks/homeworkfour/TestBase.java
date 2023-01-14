492fb35992f193983b307971983310cdea113341
ivan-andriankin <andriankin.ivan@yandex.ru>
2023-01-15 00:45:34 +0400

    practice PageObjects

---
+++ src/test/java/homeworks/homeworkfour/TestBase.java
@@ -0,0 +1,16 @@
+package homeworks.homeworkfour;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1600x800";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+}
