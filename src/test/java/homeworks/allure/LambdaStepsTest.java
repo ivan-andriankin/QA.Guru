c840701b21d530df682df55036388759ccded7dc
ivan-andriankin <andriankin.ivan@yandex.ru>
2023-01-31 11:17:10 +0400

    Allure homework

---
+++ src/test/java/homeworks/allure/LambdaStepsTest.java
@@ -0,0 +1,47 @@
+package homeworks.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class LambdaStepsTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 74;
+
+    @Test
+    public void testIssueSearch() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+
+        step("Открываем табу Issues", () -> {
+            $("#issues-tab").click();
+        });
+
+        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
+            $("#issue_" + ISSUE).shouldHave(Condition.text("69 nice"));
+        });
+
+    }
+
+}
---
+++ src/test/java/homeworks/allure/TestInvokation.java
@@ -0,0 +1,25 @@
+package homeworks.allure;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import practice.allure.WebSteps;
+
+public class TestInvokation {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 74;
+
+    @Test
+    public void testAnnotatedStep(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+    }
+
+}
